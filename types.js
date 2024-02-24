011a8c24c65e45c98e5997de38371fede6bf44cd
Ihor Karpov <igora75@gmail.com>
2024-02-24 11:59:40 +0200

    debug

--- types.js
+++ types.js
@@ -1,49 +1,48 @@
-let phone = 90.2345;
-let computer = 123.965;
-let headphone = 15.678;
+const phone = 90.2345;
+const computer = 123.965;
+const headphone = 15.678;
 
-let max = Math.max(phone,computer,headphone);
-console.log(max);
+const max = Math.max(phone,computer,headphone);
+console.log(`Максимальне значення: ${max}`);
 
-let min = Math.min(phone,computer,headphone)
-console.log(min);
+const min = Math.min(phone,computer,headphone)
+console.log(`Мінімальне значення: ${min}`);
 
-let sum = phone + computer + headphone;
-console.log(sum);
+const sum = phone + computer + headphone;
+console.log(`Сума: ${sum}`);
 
-let sumSecond = Math.floor(sum)
-console.log(sumSecond);
+const sumFloor = Math.floor(sum)
+console.log(`Сума без копійок: ${sumFloor}`);
 
-let sumThird = Math.floor(sum)
-console.log(sumThird);
+const sumFloorMin = Math.floor((sum)/100)*100
+console.log(`Округлення в меншу сторону: ${sumFloorMin}`);
 
-let sumFourth = Math.round((sum)/100)*100
-console.log(sumFourth);
+const sumFloorMax = Math.ceil((sum)/100)*100
+console.log(`Сума округлена до сотень: ${sumFloorMax}`);
 
 if (Math.floor(sum) % 2 == 0) {
     console.log(true);
 }else(console.log(false));
 
-let rest = 500 - sum
-console.log(rest);
+const rest = 500 - sum
+console.log(`Решта: ${rest}`);
 
-let midSum = (sum/3).toFixed(2)
-console.log(midSum);
+const midSum = (sum/3).toFixed(2)
+console.log(`Середнє значення цін, округлене до другого знаку після коми: ${midSum}`);
 
 // СЕРЕДНЄ
 
 let discount = Math.floor(Math.random()*101)
-console.log(discount);
+console.log(`Випадкова знижка: ${discount}`);
 
 let discountSecond = Math.random()*101
 let price = (sum - discountSecond).toFixed(2)
-console.log(price);
+console.log(`Сумa до оплати округлена до 2 знаків після коми: ${price}`);
 
 let cash = (Math.random()*501).toFixed(2) 
 let discountThird = Math.floor(Math.random()*101) 
 let changePrice = cash*discountThird/100 
 let realPrice = cash/2
 let farm = (realPrice - changePrice).toFixed(2)
-console.log(farm);
-
-console.log((`максимальне число:${max},`),(`мінімальне число:${min},`),(`сума:${sum},`),(`сума друга:${sumSecond},`),(`сума третя:${sumThird},`),(`сума четверта:${sumFourth},`),(`сума решти:${rest},`),(`середнє значення:${midSum}.`));
+console.log(`Чистий прибуток: ${farm}`);
+// console.log((`максимальне число:${max},`),(`мінімальне число:${min},`),(`сума:${sum},`),(`сума друга:${sumSecond},`),(`сума третя:${sumThird},`),(`сума четверта:${sumFourth},`),(`сума решти:${rest},`),(`середнє значення:${midSum}.`));
